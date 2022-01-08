edee463a77f905d192184608f7b3b6213cee5aae
Obbie Richardson <obbier@users.noreply.github.com>
2022-01-08 13:25:26 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -34,7 +34,7 @@ function configure_system() {
 	arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf
 	arch-chroot /mnt echo "tower" > /etc/hostname
 	arch-chroot /mnt mkinitcpio -P
-	arch-chroot /mnt pacman -S --noconfirm intel-ucode efibootmgr grub dhcpcd sudo nvidia bspwm sxhkd alacritty git
+	arch-chroot /mnt pacman -S --noconfirm intel-ucode efibootmgr grub dhcpcd sudo nvidia bspwm sxhkd alacritty git noto-fonts xorg-xrandr xwallpaper neovim github-cli
 }
 
 function install_bootloader() {
