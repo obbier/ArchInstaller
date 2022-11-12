0a600bb1710eb1265daf67b4145e5e9d545f7cbf
Obbie Richardson <obbier@users.noreply.github.com>
2022-11-12 11:33:41 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -46,7 +46,7 @@ function install_bootloader() {
 }
 
 function basic_setup() {
-	arch-chroot /mnt systemctl enable dhcpcd
+	arch-chroot /mnt systemctl enable dhcpcd reflector
 	arch-chroot /mnt pacman -S --noconfirm nvidia alsa-utils bspwm sxhkd alacritty reflector rsync git noto-fonts xorg-xrandr xwallpaper neovim github-cli
 }
 
