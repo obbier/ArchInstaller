b78c9fadc88de7592e24cc39b070977715fa7f1f
Obbie Richardson <obbier@users.noreply.github.com>
2023-11-10 10:29:09 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -62,7 +62,7 @@ function configure_system() {
     arch-chroot /mnt locale-gen
     arch-chroot /mnt touch /etc/locale.conf
     arch-chroot /mnt touch /etc/hostname
-    arch-chroot /mnt bash -c 'sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf'
+    arch-chroot /mnt bash -c 'sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 10/ /etc/pacman.conf"'
     arch-chroot /mnt bash -c 'echo "LANG=en_US.UTF-8" > /etc/locale.conf'
     arch-chroot /mnt bash -c 'echo "tower" > /etc/hostname'
     arch-chroot /mnt bash -c 'echo -e "--save /etc/pacman.d/mirrorlist\n-c us\n--sort rate\n--score 30" > /etc/xdg/reflector/reflector.conf'
